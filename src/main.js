7db890f6e784f5bab9907f7ef33fb7f50e17e5e9
Nick Andreev <bakasaru@list.ru>
2020-01-19 20:53:31 +0300

    Add questions into new test page

--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ import 'bulma-helpers/css/bulma-helpers.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
     faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
-    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload, faPlus, faMinus, faStar } from "@fortawesome/free-solid-svg-icons";
+    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload, faPlus, faMinus, faStar, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
     faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
-    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload, faPlus, faMinus, faStar);
+    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload, faPlus, faMinus, faStar, faTrash);
 Vue.component('vue-fontawesome', FontAwesomeIcon);
 
 Vue.use(Buefy, {
--- src/pages/TestAdd.vue
+++ src/pages/TestAdd.vue
@@ -44,12 +44,48 @@
                             </b-field>
                         </div>
                     </div>
+                    <div class="tile is-parent" v-for="(question, i) in questions" :key="i">
+                        <div class="tile is-child box">
+                            <p class="title is-5">Вопрос {{ i + 1 }}</p>
+                            <b-field horizontal
+                                     label="Текст вопроса">
+                                <b-input v-model="question.title"/>
+                            </b-field>
+                            <b-field horizontal
+                                     label="Баллы за вопрос">
+                                <b-numberinput v-model="question.points"
+                                               min="0"
+                                               type="is-light"/>
+                            </b-field>
+                            <b-field horizontal
+                                     label="Варианты ответа">
+                                <b-button @click="question.answers.push({ key: '', value: '' })">Добавить вариант</b-button>
+                            </b-field>
+                            <b-field v-for="(choice, i) in question.answers"
+                                     :key="i"
+                                     horizontal>
+                                <b-radio-button v-model="question.correct_answer"
+                                                :native-value="question.answers[i].key"
+                                                type="is-success">
+                                    <b-icon icon="check"/>
+                                    Верный ответ
+                                </b-radio-button>
+                                <b-input v-model="question.answers[i].key"/>
+                                <b-input v-model="question.answers[i].value"/>
+                                <b-button @click="question.answers.splice(i, 1)"
+                                          icon-left="trash"
+                                          type="is-danger">
+                                    Удалить
+                                </b-button>
+                            </b-field>
+                        </div>
+                    </div>
                 </div>
                 <div class="level">
                     <div class="level-left"></div>
                     <div class="level-right">
                         <div class="level-item">
-                            <b-button type="is-primary">
+                            <b-button type="is-primary" @click="addQuestion">
                                 <strong>Добавить вопрос</strong>
                             </b-button>
                         </div>
@@ -73,8 +109,30 @@ export default {
         description: '',
         attempt_questions: 0,
         image: null,
-        difficulty: 1
-    })
+        difficulty: 1,
+        questions: [
+
+        ]
+    }),
+    methods: {
+        addQuestion() {
+            this.questions.push({
+                title: '',
+                answers: [
+                    {
+                        key: '1',
+                        value: 'Ответ 1'
+                    },
+                    {
+                        key: '2',
+                        value: 'Ответ 2'
+                    },
+                ],
+                correct_answer: 0,
+                points: 0
+            });
+        }
+    }
 }
 </script>
 
