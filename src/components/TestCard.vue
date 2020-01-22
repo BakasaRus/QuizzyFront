e5960d704172576e4043ecdd6dc34cfe316c360b
Nick Andreev <bakasaru@list.ru>
2020-01-22 03:32:33 +0300

    Score vs protocols on Tests page

--- src/components/TestCard.vue
+++ src/components/TestCard.vue
@@ -8,11 +8,21 @@
                 </small>
             </p>
             <p class="subtitle is-6">{{ test.description }}</p>
+            <p class="subtitle is-6" v-if="test.info !== null">
+                {{ test.info.score | pluralPoints }}
+            </p>
+            <p class="subtitle is-6" v-else>
+                Тест не пройден
+            </p>
         </div>
         <div class="card-footer">
             <router-link :to="{ name: 'attempt', params: { id: test.id } }"
-                         class="card-footer-item">
+                         class="card-footer-item" v-if="test.info === null">
                 Пройти
+            </router-link>
+            <router-link :to="{ name: 'protocol', params: { id: test.id } }"
+                         class="card-footer-item" v-else>
+                Протокол
             </router-link>
             <router-link :to="{ name: 'test-stats', params: { id: test.id } }"
                          class="card-footer-item">
@@ -25,7 +35,15 @@
 <script>
 export default {
     name: "CourseCard",
-    props: ['test']
+    props: ['test'],
+    filters: {
+        pluralPoints(value) {
+            const titles = ['балл', 'балла', 'баллов'];
+            const cases = [2, 0, 1, 1, 1, 2];
+            const res = titles[(value % 100 > 4 && value % 100 < 20) ? 2 : cases[(value % 10 < 5) ? value % 10 : 5]];
+            return `${value} ${res}`;
+        }
+    }
 }
 </script>
 
